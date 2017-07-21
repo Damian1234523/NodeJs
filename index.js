17e809e1658a9cdd33d17455f06090833701e841
Damian Pistorius <dpistorius@posteo.de>
2017-07-21 18:48:19 +0200

    bla

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Dude it is working')
 })
 
-app.listen(3000, function () {
-  console.log('app listening on port 3000!')
+app.listen(9001, function () {
+  console.log('app listening on port 9001! it is over 9000!!!')
 })
